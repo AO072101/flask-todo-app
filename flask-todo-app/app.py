c6c9b7b663b991324824284ecc2800293a837b5a
AOISORA072101 <skyblue092101@gmail.com>
2018-09-09 00:37:51 +0900

    create todo-app-api-v2

--- flask-todo-app/app.py
+++ flask-todo-app/app.py
@@ -1,46 +1,21 @@
-from flask import Flask, jsonify, request
-from todo import ToDoItem, init_db, init_schema
-from todo import item_schema, items_schema
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_restless import APIManager
+from flask_cors import CORS
 
 app = Flask(__name__)
-
-db = init_db(app)
-init_schema(app)
-
-
-@app.route('/api/todoitems', methods=['GET'])
-def get_todoitems():
-    items = ToDoItem.query.all()
-    return items_schema.jsonify(items)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/todoitems.db"
+db = SQLAlchemy(app)
+manager = APIManager(app, flask_sqlalchemy_db=db)
+CORS(app)
 
 
-@app.route('/api/todoitems/<int:item_id>', methods=['GET'])
-def get_todoitem(item_id):
-    item = ToDoItem.query.filter_by(item_id=item_id).first_or_404()
-    return item_schema.jsonify(item)
+class ToDoItem(db.Model):
+    __tablename__ = "todoitems"
+    item_id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean, nullable=False, default=False)
 
 
-@app.route('/api/todoitems', methods=['POST'])
-def add_todoitem():
-    if not 'title' in request.json:
-        return 'error'
-    item = ToDoItem(title=request.json['title'], done=False)
-    db.session.add(item)
-    db.session.commit()
-    return item_schema.jsonify(item)
-
-
-@app.route('/api/todoitems/<int:item_id>', methods=['DELETE'])
-def delete_todoitem(item_id):
-    item = ToDoItem.query.filter_by(item_id=item_id).first_or_404()
-    db.session.delete(item)
-    db.session.commit()
-    return jsonify({'result': True})
-
-
-@app.route('/api/todoitems/<int:item_id>', methods=['PUT'])
-def update_todoitem(item_id):
-    item = ToDoItem.query.filter_by(item_id=item_id).first_or_404()
-    item.done = not item.done
-    db.session.commit()
-    return item_schema.jsonify(item)
+manager.create_api(ToDoItem,
+                   methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
